5b3e62ac05b4c18ff89a0b54cc3d519582af62a9
Gakawarstone <pinnekerv@gmail.com>
2022-12-07 19:28:03 +0100

    feat: solved day6

---
+++ day6/part_2.py
@@ -0,0 +1,11 @@
+with open('input') as input_file:
+    line = [l for l in input_file][0]
+
+    ch_buffer = []
+    for n, ch in enumerate(line):
+        if len(ch_buffer) == 14:
+            if len(set(ch_buffer)) == 14:
+                print(n)
+                break
+            ch_buffer.pop(0)
+        ch_buffer.append(ch)
